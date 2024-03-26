1509969c51b4914cf9537fd1c9601b838d02f8d0
8YBY8 <yubingyang9@gmail.com>
2024-03-26 20:57:04 +0800

    frontend finished

--- script.js
+++ script.js
@@ -8,19 +8,26 @@ const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
+returnMovies(APILINK); // show all movies before searching
 function returnMovies(url){
     fetch(url).then(res => res.json())
-    .then(function(data)){
+    .then(function(data){
         console.log(data.results);
         data.results.forEach(element => {
             const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
             const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
             const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
             const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
             const title = document.createElement('h3');
+            title.setAttribute('id', 'title');
             const center = document.createElement('center');
 
-            title.innerHTML = '${element.title}';
+            title.innerHTML = `${element.title}`;
             image.src = IMG_PATH + element.poster_path;
 
             center.appendChild(image);
@@ -30,7 +37,7 @@ function returnMovies(url){
             div_row.appendChild(div_column);
             main.appendChild(div_row);
         });
-    }
+    });
 }
 
 // start new search after search move, remove the old movies
@@ -44,4 +51,4 @@ form.addEventListener("submit", (e) => {
         searchItem = "";
     }
 
-})
+});

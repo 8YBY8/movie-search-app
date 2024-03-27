89f0a16ab9d3a421a72b69794c1f807461a00d48
8YBY8 <yubingyang9@gmail.com>
2024-03-27 16:49:14 +0800

    backend almost finish
    need to test

--- backend/api/reviews.route.js
+++ backend/api/reviews.route.js
@@ -4,11 +4,11 @@ import ReviewsCtrl from "./reviews.controller.js"
 const router = express.Router()
 
 // router.route("/").get((req, res) => res.send("Hello world!"))
-router.route("/move/:id").get(ReviewsCtrl.apiGetReview)
+router.route("/move/:id").get(ReviewsCtrl.apiGetReviews)
 router.route("/new").post(ReviewsCtrl.apiPostReview)
 router.route("/:id")
     .get(ReviewsCtrl.apiGetReview)
     .put(ReviewsCtrl.apiUpdateReview)
-    .delete(ReviewsCtrl.apiDeletReview)
+    .delete(ReviewsCtrl.apiDeleteReview)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ import express from "express" // javaScript package
 import cors from "cors"
 import reviews from "./api/reviews.route.js"
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/v1/reviews", reviews);
-app.use("*", (req, res) => 
-    res.status(404).json({error: "not found"}));
+app.use("/api/v1/reviews", reviews)
+app.use("*", (req, res) => res.status(404).json({error: "not found"}))
 
-export default app;
+export default app

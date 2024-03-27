fe62de57f220fe53aeb5704efceb9b8a99fe5cff
8YBY8 <yubingyang9@gmail.com>
2024-03-27 11:48:07 +0800

    backend draft 1

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+// database code 
+// mongodb+srv://yubingyang9:13706854306Yu@cluster0.itvqll5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewDAO from "./dao/reviewsDAO.js"
+
+const MonhoClient

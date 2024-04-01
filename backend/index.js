5defe5af59aa867a38f021a4b04306d9f70a2e3a
8YBY8 <yubingyang9@gmail.com>
2024-04-01 14:13:49 +0800

    ff

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,24 @@ import mongodb from "mongodb"
 import ReviewDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env["MONGODB_USERNAME"]
-console.log(mongo_username)
-const mongo_password = process.env["MONGODB_PASSWORD"]
-console.log(mongo_password)
+// const mongo_username = process.env["MONGODB_USERNAME"]
+// console.log(mongo_username)
+// const mongo_password = process.env["MONGODB_PASSWORD"]
+// console.log(mongo_password)
 // const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.itvqll5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const uri = `mongodb+srv://yubingyang9:13706854306Yu@cluster0.itvqll5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// mongodb+srv://yubingyang9:<password>@cluster0.itvqll5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 const port = 8000
 
+console.log("connecting " + uri)
 MongoClient.connect(
     uri,
     {
         maxPoolSize: 50,
         wtimeoutMS: 2500,
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        // console.log("connected!!!")
     })
     // catch any error happens when you connect
     .catch(err => {
@@ -28,7 +31,7 @@ MongoClient.connect(
         process.exit(1) // ending the program
     })
     .then(async client => {
-        await ReviewDAO.injectDB(client)
+        // await ReviewDAO.injectDB(client)
         app.listen(port, () => {
             console.log(`listening on port ${port}`)
         })

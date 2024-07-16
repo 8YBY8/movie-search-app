c31b6d934ae9d273bb68605069e55d0f0ac8baaa
8YBY8 <156168881+8YBY8@users.noreply.github.com>
2024-07-16 15:48:32 +0800

    Update reviews.route.js fix the typo

--- backend/api/reviews.route.js
+++ backend/api/reviews.route.js
@@ -4,7 +4,7 @@ import ReviewsCtrl from "./reviews.controller.js"
 const router = express.Router()
 
 // router.route("/").get((req, res) => res.send("Hello world!"))
-router.route("/move/:id").get(ReviewsCtrl.apiGetReviews)
+router.route("/movie/:id").get(ReviewsCtrl.apiGetReviews)
 router.route("/new").post(ReviewsCtrl.apiPostReview)
 router.route("/:id")
     .get(ReviewsCtrl.apiGetReview)

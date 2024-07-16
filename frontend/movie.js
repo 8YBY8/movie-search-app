bbb318ae60677d86c1df3e03be2b603245448aeb
8YBY8 <156168881+8YBY8@users.noreply.github.com>
2024-07-16 15:54:55 +0800

    Update movie.js

--- frontend/movie.js
+++ frontend/movie.js
@@ -5,7 +5,8 @@ const movieTitle = url.searchParams.get("title")
 // input the API
 // doc: https://developer.themoviedb.org/docs/append-to-response
 // const APILINK = 'http://127.0.0.1:5500/backend/api/v1/reviews/';
-const APILINK = '../backend/api/v1/reviews/';
+// const APILINK = '../backend/api/v1/reviews/';
+const APILINK = 'http://localhost:5000/api/v1/reviews/';
 
 
 const main = document.getElementById("section");

a9e89a463ba4d2461e3231dbab19ba0d09d1a94c
8YBY8 <yubingyang9@gmail.com>
2024-08-23 12:28:37 +0800

    change backend server

--- frontend/movie.js
+++ frontend/movie.js
@@ -6,8 +6,9 @@ const movieTitle = url.searchParams.get("title")
 // doc: https://developer.themoviedb.org/docs/append-to-response
 // const APILINK = 'http://127.0.0.1:5500/backend/api/v1/reviews/';
 // const APILINK = '../backend/api/v1/reviews/';
-// const APILINK = 'http://localhost:5000/api/v1/reviews/';
-const APILINK = 'https://3aa70ddf-379a-4101-a9c3-213ae8ff63e1-00-8akbmliqyvsb.picard.replit.dev/api/v1/reviews/';
+const APILINK = 'https://movie-search-app-22pv.onrender.com/api/v1/reviews/';
+// const APILINK = 'https://3aa70ddf-379a-4101-a9c3-213ae8ff63e1-00-8akbmliqyvsb.picard.replit.dev/api/v1/reviews/';
+// const APILINK = 'https://8yby8.github.io/movie-search-app-backend/api/v1/reviews/';
 
 
 const main = document.getElementById("section");
